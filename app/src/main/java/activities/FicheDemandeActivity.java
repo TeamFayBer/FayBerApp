7902f4462b2c5ecd262846dd326f5af34551a293
Sabert <lomontrandsabert@yahoo.fr>
2017-08-26 15:21:07 -0300

    Update XML

--- app/src/main/java/activities/FicheDemandeActivity.java
+++ app/src/main/java/activities/FicheDemandeActivity.java
@@ -5,11 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import codepath.fayberapp.R;
 
-public class FicheDemandeActivity extends AppCompatActivity {
+import static android.os.Build.VERSION_CODES.M;
 
+public class FicheDemandeActivity extends AppCompatActivity implements OnItemSelectedListener {
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +27,41 @@ public class FicheDemandeActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        // Spinner element
+        spinner = (Spinner) findViewById(R.id.spiSexe);
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Sexe");
+        categories.add("M");
+        categories.add("F");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+        if(item.equals("Sexe")){
+        }
+    }
+
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -29,8 +73,12 @@ public class FicheDemandeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     public void onLogButton(View v) {
-        Intent i = new Intent(FicheDemandeActivity.this, ResponseActivity.class);
-        startActivity(i);
+        if(spinner.getSelectedItem().toString()!="Sexe"){
+            Intent i = new Intent(FicheDemandeActivity.this, ResponseActivity.class);
+            startActivity(i);
+        }else{
+            Toast.makeText(this, "Select sex", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
